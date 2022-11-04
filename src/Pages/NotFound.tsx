648ceb1902894e1e389da761915354be1bb4a870
Grace de la Mora <thetrend@users.noreply.github.com>
2022-11-04 10:15:05 -0400

    feat(hygraph): create an authenticated user dashboard

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1>Error: Page Not Found</h1>
+      <p>The page you were looking for does not seem to exist.</p>
+    </>
+  )
+}
+
+export default NotFound;
