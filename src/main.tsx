ed2d1fb27b701dcbd09db2baddd5c8d69b43d86a
Grace de la Mora <thetrend@users.noreply.github.com>
2022-11-03 22:53:21 -0400

    fix(auth): Move env variables to Netlify

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Auth0Provider 
-      domain={import.meta.env.AUTH0_DOMAIN}
-      clientId={import.meta.env.AUTH0_CLIENT_ID}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
     >
       <App />
