12b6a66c35fd93094e88544cefe9d59c430b01e8
Grace de la Mora <thetrend@users.noreply.github.com>
2022-11-03 21:42:43 -0400

    feat(auth0): implementing basic authentication via auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import blogLogo from './assets/blog-logo.svg';
+import { useAuth0 } from '@auth0/auth0-react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <img src={blogLogo} />
+      {!isAuthenticated
+        ? <a onClick={() => loginWithRedirect()}>Login</a>
+        : 
+          <>
+            Hello, {user?.name}. <a onClick={() => logout({ returnTo: window.location.origin })}>Logout</a>
+          </>
+          }
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
